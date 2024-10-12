636505b795df5f2804ccb7a090f0da70387ada26
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2024-10-12 16:09:02 -0500

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // base: 'https://yfdd617235.github.io/giic/', // Ruta base relativa
-  base: '/giic/', // Ruta base relativa
-});
-
+// For github pages
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 
 // export default defineConfig({
 //   plugins: [react()],
-//   base: '/', //Dominio personalizado
+//   base: '/giic/', // cuando se tiene en github (base relativa)
 // });
+
+
+
+// For customised domains:
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', //cuando se tiene un Dominio personalizado
+});
